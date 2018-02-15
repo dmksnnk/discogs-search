1776b0ea40f5a53cd3a4d934e69c42b9c865a120
aspidima <kasyanenkodg@gmail.com>
2018-02-15 22:40:55 +0100

    Add check for Ratelimit and other status codes

--- discogs_search/client.py
+++ discogs_search/client.py
@@ -1,7 +1,21 @@
+import logging
+import time
 import requests
+
+logger = logging.getLogger('client')
 
 
 class DiscogsClient():
+    class RequestFailedError(Exception):
+        def _init__(self, params, code):
+            self.params = params
+            self.code = code
+
+        def __repr__(self):
+            return 'Request with params {} failed ({})'.format(
+                    self.params, self.code)
+
+
     def __init__(self, name, token):
         self.name = name
         self._token = token
@@ -20,15 +34,17 @@ class DiscogsClient():
         :rtype: list or None
         """
         url = 'https://api.discogs.com/artists/{}/releases'.format(artist_id)
-        releases = self._get(url)
+        releases = self._get_request(url)
         return releases.get('releases')
 
     def _search(self, req, type):
         params = {'q': req,
                   'type': type}
-        return self._get('https://api.discogs.com/database/search', params)
+        return self._get_request('https://api.discogs.com/database/search', params)
 
-    def _get(self, url, params=None):
+    def _get_request(self, url, params=None):
+        """Sends GET request to """
+        logger.debug('Sending request with params %s', params)
         headers = {'user-agent': self.name}
         # if params aren't set
         if params is None:
@@ -37,7 +53,25 @@ class DiscogsClient():
             params.update({'token': self._token})
 
         resp = requests.get(url, headers=headers, params=params)
+        logger.debug('Receive response code %s, remaining %s requests',
+                     resp.status_code, resp.headers.get('X-Discogs-Ratelimit-Remaining'))
+
+        if resp.status_code not in [200, 204, 429]:
+            raise self.RequestFailedError(params, resp.status_code)
+
+
+        rate_limit = resp.headers.get('X-Discogs-Ratelimit-Remaining')
+        if rate_limit and int(rate_limit) <= 1:
+            retry_time = resp.headers.get('Retry-After')
+            if retry_time:
+                retry_time = int(retry_time)
+                logger.warning('Reach Ratelimit, waiting for %s s', retry_time)
+                time.sleep(retry_time)
+            else:
+                logger.error('Did not get Retry-After time')
+
+
         try:
-            return  resp.json()
+            return resp.json()
         except ValueError:
-            return None
+            return {}
